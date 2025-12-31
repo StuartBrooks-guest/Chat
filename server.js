de2ffab419c9d68bd6593e1d6e95835a3e1fd322
StuartBrooks-guest <35343050+StuartBrooks-guest@users.noreply.github.com>
2025-12-31 11:29:50 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,30 @@
-const WebSocket = require("ws");
+const WebSocket = require('ws');
+const http = require('http');
 
-const wss = new WebSocket.Server({ port: process.env.PORT || 8080 });
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('WebSocket signaling server running');
+});
+
+const wss = new WebSocket.Server({ server });
+
 let clients = [];
-
-wss.on("connection", ws => {
+wss.on('connection', ws => {
   clients.push(ws);
 
-  ws.on("message", msg => {
-    for (const c of clients) {
+  ws.on('message', msg => {
+    clients.forEach(c => {
       if (c !== ws && c.readyState === WebSocket.OPEN) {
         c.send(msg);
       }
-    }
+    });
   });
 
-  ws.on("close", () => {
+  ws.on('close', () => {
     clients = clients.filter(c => c !== ws);
   });
 });
+
+// Use Railway-assigned port
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
